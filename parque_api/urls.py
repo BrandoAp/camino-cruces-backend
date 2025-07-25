a09a90194e18e7765d019bf16995cd7d21e3bcb2
BrandoAp <avilabrando14@gmail.com>
2025-07-24 22:26:26 -0500

    Update urls.py

--- parque_api/urls.py
+++ parque_api/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Cambia 'mi_app' por tu app
+    path('api/', include('api.urls')),  # Asegúrate que api/urls.py existe
+    path('', lambda request: JsonResponse({'status': 'ok'})),  # <- Ruta raíz
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
