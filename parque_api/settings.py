fd5b8d372c109998359c2feae1799d651f6ed412
BrandoAp <avilabrando14@gmail.com>
2025-07-24 22:37:11 -0500

    Update settings.py ALLOWED_HOST variable

--- parque_api/settings.py
+++ parque_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')  # Lee la clave desde .env
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)  # Lee DEBUG desde .env
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=lambda v: v.split(','))
+ALLOWED_HOSTS = ['.railway.app']
 
 
 # Application definition
